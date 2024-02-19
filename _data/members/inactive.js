6030a01011a004b6b7544bc8943a8dd0dbfbfb7f
Dhruv Shah <20je0326@mech.iitism.ac.in>
2024-02-20 01:35:18 +0530

    updated current team about and alumni details

---
+++ _data/members/inactive.js
@@ -0,0 +1,18 @@
+export const inactive = [
+    {
+        "name": "Tanishq Chaudhary",
+        "position": "Final Year",
+        "image": "/images/members/sophomores/Tanishq Chaudhary.jpg",
+        "linkedin": "https://www.linkedin.com/in/tanishq-chaudhary-1018241b1/",
+        "github": "https://github.com/Tanishq30052002",
+        "email": "20je1015@ece.iitism.ac.in"
+    },
+    {
+        "name": "Ayushman Tiwari",
+        "position": "Final Year",
+        "image": "/images/members/sophomores/Ayushman Tiwari.jpg",
+        "linkedin": "https://www.linkedin.com/in/ayushman-tiwari-9757871b9/",
+        "github": "https://github.com/Ayuschmannov",
+        "email": "20je0241@me.iitism.ac.in"
+    },
+]
